2fbfc87dacfd07cdbfbcdfb32fa1cb7606120f55
Forest Eckhardt <forest.eckhardt@docker.com>
2026-03-24 17:35:09 -0400

    jruby: patches CVE-2026-33210 (#32776)
    * jruby: patches CVE-2026-33210
    
    * jruby: bump gems in alpine
    
    * jruby: patches to avoid substring matches
    
    [dhi-commit=21653dd8bafbeebb0fd9ba02dae133a39a1f5db2]

--- image/jruby/patch/patch.sh
+++ image/jruby/patch/patch.sh
@@ -3,9 +3,9 @@ NAME=$1
 VERSION=$2
 
 REGEX="([0-9]+\.[0-9]+\.[0-9]+)"
-if [[ "$(grep $NAME pom.rb)" =~ ([0-9]+\.[0-9]+\.[0-9]+) ]]; then
+if [[ "$(grep "'$NAME'" pom.rb)" =~ ([0-9]+\.[0-9]+\.[0-9]+) ]]; then
     CUR_VERSION="${BASH_REMATCH[0]}"
     if [[ "$(printf '%s\n' "$CUR_VERSION" "$VERSION" | sort -V | head -n1)" = "$CUR_VERSION" ]]; then
-    sed -i "s/.*$NAME.*/  ['$NAME', '$VERSION'],/g" pom.rb
+    sed -i "s/^  \['$NAME', '[^']*'\],$/  ['$NAME', '$VERSION'],/g" pom.rb
     fi
 fi
